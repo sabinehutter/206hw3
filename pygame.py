aaec465f63a377f6fd51c74bbe09f5c5cf36f371
Sabine Hutter <shutter@umich.edu>
2016-11-20 18:06:59 -0500

    commit

--- pygame.py
+++ pygame.py
@@ -1,2 +1,20 @@
 #Due December 2
-#this is my pygame file
+import pygame
+pygame.init();
+
+#creating initial colors
+white = (255,255,255)
+black = (0,0,0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+pink = (255, 153, 204)
+
+#initalizing position variables
+x_pos = 0
+y_pos = 0
+
+#creating a screen using a tuple
+gameDisplay = pygame.display.set_mode((800,600))
+#Naming the screen that is created
+pygame.display.set_caption("Sabine's Pygame Example")
