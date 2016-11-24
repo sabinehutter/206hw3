3c81df8f2b128e220e0cc649cff78233810abead
Sabine Hutter <shutter@umich.edu>
2016-11-24 17:07:46 -0500

    Sprites and Pictures added

--- pygame.py
+++ pygame.py
@@ -1,7 +1,10 @@
 #Due December 2
 import pygame
-pygame.init();
+from pygame import *
+from pygame.sprite import *
+from random import *
 
+DELAY = 1000;
 #creating initial colors
 white = (255,255,255)
 black = (0,0,0)
@@ -14,12 +17,91 @@ pink = (255, 153, 204)
 x_pos = 0
 y_pos = 0
 
-#creating a screen using a tuple
-gameDisplay = pygame.display.set_mode((800,600))
+
 #Naming the screen that appears to 
 pygame.display.set_caption("Sabine's Pygame Example")
 #Close the screen using a tuple
+
+class Flower(Sprite):
+	def __int__(self):
+		Sprite.__int__(self)
+		self.image = image.load("Flower.jpeg").comvert_alpha()
+		self.rect = self.image.get_rect()
+	def move(self):
+		randX = randint(0,600)
+		randY = randint(0,400)
+		self.rect.center = (randX, randY)
+class Water_Can(Sprite):
+	def __int__(self):
+		Sprite.__int__(self)
+		self.image = image.load("watercan.jpeg").comvert_alpha()
+		self.rect = self.image.get_rect()
+	def hit(self, target):
+		return self.rect.colliderect(target)
+
+init()
+screen_create = display.set_mode((640,480))
+display.set_caption("Water the Flowers")
 mouse.set_visible(False)
-lilo = Lilo()
-food = Food()
-sprites = RenderPlain(lilo, food)
+
+
+flower = Flower()
+can = Water_Can()
+
+sprites = RenderPlain(flower, can)
+
+while True:
+    e = event.poll()
+    if e.type == QUIT:
+        quit()
+        break
+
+    elif e.type == MOUSEBUTTONDOWN:
+        if shovel.hit(gold):
+            mixer.Sound("cha-ching.wav").play()
+            gold.move()
+            hits += 1
+
+            # reset timer
+            time.set_timer(USEREVENT + 1, DELAY)
+            
+    elif e.type == USEREVENT + 1: # TIME has passed
+        gold.move()
+
+    # refill background color so that we can paint sprites in new locations
+    screen.fill(bgcolor)
+    t = f.render("Jackpot = " + str(hits), False, (0,0,0))
+    screen.blit(t, (320, 0))        # draw text to screen.  Can you move it?
+
+    # update and redraw sprites
+    sprites.update()
+    sprites.draw(screen)
+    display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
